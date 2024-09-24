36e0314bbf36400e313c1d18d7abd9e8d6e5dc87
safia <safiaahmed1786@gmail.com>
2024-09-24 10:27:30 +0600

    protoType last

---
+++ protoType.js
@@ -0,0 +1,26 @@
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// You may assume the array is the output of JSON.parse.
+
+// Example 1:
+
+// Input: nums = [null, {}, 3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+// Example 2:
+
+// Input: nums = []
+// Output: -1
+// Explanation: Because there are no elements, return -1.
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+  console.log(this[this.length - 1])
+  if (this.length>0 ) return this[this.length - 1];
+  else return -1;
+};
+
+const arr = [null,{}];
+console.log(arr.last()); // 3
