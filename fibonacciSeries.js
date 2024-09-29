95ba04d32b5a2cdf39586080a81fb795c6038438
safia <safiaahmed1786@gmail.com>
2024-09-29 09:59:38 +0600

    fibonacci with generator

---
+++ fibonacciSeries.js
@@ -0,0 +1,35 @@
+// Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+// Example 1:
+
+// Input: callCount = 5
+// Output: [0,1,1,2,3]
+// Explanation:
+// const gen = fibGenerator();
+// gen.next().value; // 0
+// gen.next().value; // 1
+// gen.next().value; // 1
+// gen.next().value; // 2
+// gen.next().value; // 3
+// Example 2:
+
+// Input: callCount = 0
+// Output: []
+// Explanation: gen.next() is never called so nothing is outputted
+var fibGenerator = function* () {
+  let a = 0,
+    b = 1,
+    next;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+};
+
+const gen = fibGenerator();
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
