7933d321d626daf2f9e99f6d5a58cd90b6d71566
safia <safiaahmed1786@gmail.com>
2024-10-06 10:26:13 +0600

    sumtwo up to target

--- sum2.js
+++ sum2.js
@@ -20,5 +20,13 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 var twoSum = function(nums, target) {
-   console.log( nums.sort((a,b)=>b-a).filter(num=>num<target))
-};twoSum([2,7,11,15],9)
+   for(let i=0; i<nums.length;i++){
+   for(let j=i+1; j<nums.length;j++){
+        if(nums[i]+nums[j]===target) {
+           return [i,j]
+        } 
+   }
+    
+   }
+   return []
+};
