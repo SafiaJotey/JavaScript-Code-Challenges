491b521c0da9b30629be66d8bf02c4c27e9ca0fc
safia <safiaahmed1786@gmail.com>
2024-10-15 12:57:40 +0600

    pascal triangle

---
+++ pascalTriangle2.js
@@ -0,0 +1,31 @@
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+
+// [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+var getRow = function (rowIndex) {
+  const triangle = [];
+
+  for (let i = 0; i <= rowIndex; i++) {
+    let row = new Array(i + 1).fill(1);
+    for (let j = 1; j < i; j++) {
+      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    }
+    triangle.push(row);
+  }
+  return triangle[rowIndex];
+};
+console.log(getRow(3));
